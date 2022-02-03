90a5758256f0b064dc667dcd8b6111f9f2a6a92b
Kim Conrad <90888612+kimconrad@users.noreply.github.com>
2022-02-02 19:10:23 -0800

    adding queries folder and new sql challenge file

---
+++ queries/employee_database_challenge.sql
@@ -0,0 +1,42 @@
+-- Use Dictinct with Orderby to remove duplicate rows
+-- Challenge Starter Code
+--SELECT DISTINCT ON (______) _____,
+--______,
+--______,
+--______
+--INTO nameyourtable
+--FROM _______
+--WHERE _______
+--ORDER BY _____, _____ DESC;
+
+--Challenge Deliverable 1: Table 1 retirement_titles
+SELECT e.emp_no,
+e.first_name,
+e.last_name,
+ti.title,
+ti.from_date,
+ti.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ti
+ON e.emp_no = ti.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31');
+
+--Challenge Deliverable 1: Table 2 unique_titles
+SELECT DISTINCT ON (emp_no) 
+rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY emp_no, to_date DESC;
+
+
+--Challenge Deliverable 1: Table 3 retiring_titles
+SELECT COUNT(ut.emp_no), 
+ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY COUNT DESC;
